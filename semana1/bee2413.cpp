7ef04fb8330f6703e9133380549c91b48d6de5fb
Felipe <luiz.fma.up@gmail.com>
2024-06-30 13:00:33 -0300

    first commit

---
+++ semana1/bee2413.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int clicksTerceiroLink;
+    cin >> clicksTerceiroLink;
+    cout << clicksTerceiroLink*4 << endl;
+    return 0;
+}
